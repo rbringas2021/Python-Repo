1607a35551c63e06c93f2ca2dee3aff9f52f2de4
rbringas2021 <96887472+rbringas2021@users.noreply.github.com>
2024-04-03 01:33:25 -0300

    Delete Operadores_Compuestos.py

--- Operadores_Compuestos.py
+++
@@ -1,9 +0,0 @@
-nombre = input("Ingrese su nombre: ")
-edad = int(input("Ingrese su edad: "))
-
-edad += 1
-edad = edad + 1
-
-print(f'Hola {nombre}, el año que viene tendrás {edad} años')
-
-#__END__.
